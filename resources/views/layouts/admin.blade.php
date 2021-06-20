88339c0a46ab24302a37fc871a187478232bc150
Niloy Quazi <niloy940@gmail.com>
2021-06-20 10:23:52 +0600

    fixing date time error

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,10 +11,7 @@
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('sbadmin/styles.css') }}" rel="stylesheet" />
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"
-        integrity="sha512-aEe/ZxePawj0+G2R+AaIxgrQuKT68I28qh+wgLrcAJOz3rxCP+TwrK5SPN+E5I+1IQjNtcfvb96HDagwrKRdBw=="
-        crossorigin="anonymous" />
+    <link href="{{ asset('datetime-picker/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" />
 </head>
 
 <body class="sb-nav-fixed">
@@ -85,28 +82,29 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous">
     </script>
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script> --}}
     <script src="{{ asset('sbadmin/scripts.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
         integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="
         crossorigin="anonymous"></script>
-    <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"
-        integrity="sha512-GDey37RZAxFkpFeJorEUwNoIbkTwsyC736KNSYucu1WJWFK9qTdzYub8ATxktr6Dwke7nbFaioypzbDOQykoRg=="
-        crossorigin="anonymous"></script>
+
+    <script src="{{ asset('datetime-picker/bootstrap-datetimepicker.min.js') }}"></script>
 
     <script>
         $(document).ready(function() {
             $('#datetimepicker').datetimepicker({
-                format: 'Y-MM-DD h:m:s'
+                format: 'Y-MM-DD H:m:s',
+                icons: {
+                    time: 'fa fa-clock',
+                }
             })
         });
 
         $(document).ready(function() {
             $('#datetimepicker1').datetimepicker({
-                format: 'Y-MM-DD h:m:s'
+                format: 'Y-MM-DD H:m:s',
+                icons: {
+                    time: 'fa fa-clock',
+                }
             })
         });
 
